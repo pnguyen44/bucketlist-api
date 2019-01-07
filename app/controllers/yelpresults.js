147df66fca9a6ef5f1f026f3977f1ce2ef1bbf28
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-01-07 00:44:35 -0500

    Update index function

--- app/controllers/yelpresults.js
+++ app/controllers/yelpresults.js
@@ -1,36 +1,61 @@
 'use strict'
+const request = require('request')
+const apiKey = process.env.API_KEY
 
 const controller = require('lib/wiring/controller')
-const models = require('app/models')
-const Yelpresult = models.yelpresult
+// const models = require('app/models')
+// const Yelpresult = models.yelpresult
 
-const authenticate = require('./concerns/authenticate')
-const setUser = require('./concerns/set-current-user')
-const setModel = require('./concerns/set-mongoose-model')
+// const authenticate = require('./concerns/authenticate')
+// const setUser = require('./concerns/set-current-user')
+// const setModel = require('./concerns/set-mongoose-model')
 
-const yelp = require('yelp-fusion')
+// const yelp = require('yelp-fusion')
 
 const index = (req, res, next) => {
-  const clientId = process.env.CLIENT_ID
-  const clientSecret = process.env.CLIENT_SECRET
-  yelp.accessToken(clientId, clientSecret)
-    .then(result => {
-      const client = yelp.client(result.jsonBody.access_token)
+  const options = { method: 'GET',
+    url: 'https://api.yelp.com/v3/businesses/search',
+    qs: { term: req.query.term, location: req.query.location },
+    headers: {
+       // 'cache-control': 'no-cache',
+      Authorization: 'Bearer ' + apiKey }
+  }
 
-      client.search({
-        term: req.query.term,
-        location: req.query.location
-      })
-      .then(result => {
-        const yelpresults = result.jsonBody.businesses
-        res.json({
-          yelpresults: yelpresults
-        })
-      })
-      .catch(() => res.sendStatus(400))
+  request(options, function (error, response, body) {
+    if (error) {
+      response.sendStatus(400)
+    }
+
+    const obj = JSON.parse(body)
+    const yelpresults = obj.businesses
+    // console.log('test' , yelpresults[0])
+    res.json({
+      yelpresults: yelpresults
     })
-    .catch(() => res.sendStatus(400))
+  })
 }
+
+// const index = (req, res, next) => {
+//   const clientId = process.env.CLIENT_ID
+//   const clientSecret = process.env.CLIENT_SECRET
+//   yelp.accessToken(clientId, clientSecret)
+//     .then(result => {
+//       const client = yelp.client(result.jsonBody.access_token)
+//
+//       client.search({
+//         term: req.query.term,
+//         location: req.query.location
+//       })
+//       .then(result => {
+//         const yelpresults = result.jsonBody.businesses
+//         res.json({
+//           yelpresults: yelpresults
+//         })
+//       })
+//       .catch(() => res.sendStatus(400))
+//     })
+//     .catch(() => res.sendStatus(400))
+// }
 
 const show = (req, res) => {
   res.json({
